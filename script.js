af4cc921d833dfc62b4922f6c3936fb86b14e71c
Yinson Tso <yinson.tso@gmail.com>
2023-03-14 04:40:47 -0400

    adding event

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const h2 = document.createElement('h2')
 h2.textContent = "This content added by JavaScrpt"
 
 document.querySelector('body').appendChild(h2);
+//mouse over event, if goes over section, change it's bg-color
+
+// addEventListener("mouseover", (event) => {});
+
+// onmouseover = (event) => {};
+
+let section = document.getElementById('section')
+
+section.addEventListener(
+    'mouseover',
+    (event) => {
+        section.background = 'black';
+    }
+)
